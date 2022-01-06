43072c59ea47cde52e9f0d31a6d37b501e664ae5
gaiamartin <96789536+gaiamartin@users.noreply.github.com>
2022-01-06 09:09:57 +0100

    Update bibli_Monopoly.h
    bibli avec nouvelles fonctions ( fonction jeu + fonction premier tour + fonction page de garde + fonction menu)

--- monopoly - preparation/bibli_Monopoly.h
+++ monopoly - preparation/bibli_Monopoly.h
@@ -6,9 +6,13 @@ int case4();
 void carteChance(int argentJ, int positionJ, int nbJoueurs);
 int fctnD2(int d2);
 int fctnD1(int d1);
-int fctnDD();
+int fctnDD(char nom[14]);
 void grille();
 void gotoligcol(int lig, int col);
+void fctnJeu( char nom[14], int position, int argent, int nbJoueurs);
+void premierTour( char nom[14], int position, int argent, int nbJoueurs);
+int pageDeGarde();
+int menu();
 
 
 #endif // BIBLI_MONOPOLY_H_INCLUDED
